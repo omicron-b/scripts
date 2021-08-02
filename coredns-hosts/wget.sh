040b0523fed40e6efc6e9a6852e6d45d674c0b87
Thomas Anderson <32303844+omicron-b@users.noreply.github.com>
2021-08-02 23:29:34 +0300

    - create branch ubuntu

--- coredns-hosts/wget.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-wget -i input-hosts.urls -O raw-hosts.txt
-wget -i input-simple.urls -O raw-simple.txt
--- opt/chrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lxc start apps || true
-lxc exec apps -- sudo --user ubuntu --login google-chrome-stable
